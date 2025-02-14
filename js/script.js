32a61496a26cc8788941d355ed343b5b0f9f4349
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2025-02-14 09:30:39 -0300

    codigo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initFormValidation from "./modules/initFormValidation.js";
 import initLoadInputName from "./modules/initLoadInputName.js";
 import initBannerCarousel from "./modules/initBannerCarousel.js";
+import initRatingCarousel from "./modules/initRatingCarousel.js";
 
 initFormValidation();
 initLoadInputName();
 initBannerCarousel();
+initRatingCarousel();
